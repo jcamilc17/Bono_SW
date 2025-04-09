835eef3cdaea3b3b7f5765c9ac7a812f09ece4f7
Juan Caldas <jc.caldas23@uniandes.edu.co>
2025-04-09 18:45:41 -0500

    🚀 Implementacion inicial de Bono

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgressbarComponent } from './progressbar/progressbar.component';
+import { FormComponent } from './form/form.component';
+import { NavigationBar } from './navigationbar/navigationbar';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    NavigationbarComponent,
+      ProgressbarComponent,
+      FormComponent,
+      
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/navigationbar/navigationbar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationbarComponent } from './navigationbar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NavigationbarComponent]
+})
+export class NavigationbarModule { }
